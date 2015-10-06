021bee8d8ae2227f7359a67ee49ba4c7c4e5e18b
Miika Hänninen <miika.hanninen@gmail.com>
2015-10-06 14:46:31 +0300

    Improve xmonad fullscreen handling, add gpg-agent

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,6 +1,8 @@
 import XMonad
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.DynamicLog
+import XMonad.Layout.NoBorders
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
@@ -9,13 +11,17 @@ main = do
     spawn "trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --transparent true --tint 0x191970 --height 15"
     spawn "nm-applet --sm-disable"
     spawn "gnome-power-manager"
+    spawn "gpg-agent --enable-ssh-support --daemon"
     xmproc <- spawnPipe "xmobar"
     xmonad $ defaultConfig {
-           manageHook = manageDocks <+> manageHook defaultConfig,
-           layoutHook = avoidStruts  $  layoutHook defaultConfig,
+           manageHook = composeAll [ manageDocks,
+                                     isFullscreen --> doFullFloat,
+                                     manageHook defaultConfig
+                                   ],
+           layoutHook = smartBorders . avoidStruts  $  layoutHook defaultConfig,
            logHook = dynamicLogWithPP xmobarPP {
                      ppOutput = hPutStrLn xmproc,
-                     ppTitle = xmobarColor "green" "" . shorten 150
+                     ppTitle = xmobarColor "green" "" . shorten 100
                      },
            modMask = mod4Mask
            } `additionalKeys`
